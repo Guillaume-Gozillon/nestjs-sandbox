c185f9a26400f5cba42cafdb3d410e06a09dbc6e
guillaumegozillon <guillaumegozillon@gmail.com>
2023-05-20 21:21:50 +0200

    connect postgres db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ExampleController } from './example/example.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      entities: [DatabaseModule],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([DatabaseModule]),
+  ],
+  controllers: [AppController, ExampleController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/example/example.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get } from '@nestjs/common';
+import { Connection } from 'typeorm';
+
+@Controller('example')
+export class ExampleController {
+  // eslint-disable-next-line prettier/prettier
+  constructor(private readonly connection: Connection) { }
+
+  @Get()
+  async checkConnection(): Promise<string> {
+    try {
+      await this.connection.query('SELECT 1');
+      return 'Connected to the database';
+    } catch (error) {
+      return 'Failed to connect to the database';
+    }
+  }
+}
